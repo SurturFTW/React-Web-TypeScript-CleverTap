9bb4bfadeaaa2ed72286a89989d0cc3b02b1900a
SurturFTW <pushkarsane07@gmail.com>
2025-12-10 12:40:07 +0530

    /*useEffect CT init*/

--- src/index.tsx
+++ src/index.tsx
@@ -2,15 +2,19 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./App";
-import HomePage from "./ProductExperiences/homePage";
+
 import "./index.css";
+
+// import HomePage from "./ProductExperiences/paymentPage";
+// import PaymentPage from "./ProductExperiences/paymentPage";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
-        <Route path="/home" element={<HomePage />} />
+        <Route path="/home" element={<App />} />
+        {/* <Route path="/payment" element={<PaymentPage />} /> */}
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
